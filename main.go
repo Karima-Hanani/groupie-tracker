dcc2c90bb9847421a8b97b045a6eddccadbb966c
khanani <karimahanani398@gmail.com>
2026-01-09 14:56:55 +0100

    project structure

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	fmt.Println("http://localhost:8080/")
+	err := http.ListenAndServe(":8080",nil)
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
+}
