6c86dce1a14adfd9db84e02e254ae70dac2f3bbc
khanani <karimahanani398@gmail.com>
2026-01-10 17:09:21 +0100

    fixing superfluous errors

--- handler/homehandler.go
+++ handler/homehandler.go
@@ -1,17 +1,17 @@
 package handler
 
 import (
+	"bytes"
+	"html/template"
 	"net/http"
 
 	"groupie-tracker/fetcher"
-	"html/template"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-
 	// check for path
 	if r.URL.Path != "/" {
-		ErrorPage(w,r, "Page not found.", 404)
+		ErrorPage(w, r, "Page not found.", 404)
 		return
 	}
 
@@ -23,21 +23,22 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 
 	artists, err := fetcher.FetchArtists()
 	if err != nil {
-		ErrorPage(w,r, "Failed to load artists.", 500)
-		return
-	}
-	
-	tmpl, err := template.ParseFiles("template/index.html")
-	if err != nil {
-		ErrorPage(w,r, "Failed to load template.", 500)
+		ErrorPage(w, r, "Failed to load artists.", 500)
 		return
 	}
 
-	// Render the homepage template with the fetched data
-	err = tmpl.ExecuteTemplate(w, "index.html",artists)
-	
+	tmpl, err := template.ParseFiles("template/index.html")
 	if err != nil {
-		ErrorPage(w,r, "Failed to render template.", 500)
+		ErrorPage(w, r, "Failed to load template.", 500)
 		return
 	}
+
+	var buff bytes.Buffer
+	// Render the homepage template with the fetched data
+	err = tmpl.ExecuteTemplate(&buff, "index.html", artists)
+	if err != nil {
+		ErrorPage(w, r, "Failed to render template.", 500)
+		return
+	}
+	w.Write(buff.Bytes())
 }
